42534e0c1b98669636c336701ca50aeae1785a9c
Hamza NOURSAID <hamza.noursaid@gmail.com>
2022-04-07 15:29:57 +0200

    fix test file

--- expression/functions_map_test.go
+++ expression/functions_map_test.go
@@ -49,40 +49,4 @@ func TestFlatMap(t *testing.T){
 		t.Error(err)
 		t.Logf("Result: %d, Expected: %d", val.(int), 12)
 	}
-
-	res, err = flatMap(	
-		"key",	
-		"aggs.doc_count.value",
-		[]map[string]interface{}{{
-			"key": "2022-02-24T05:00:00.000",
-			"aggs": map[string]interface{}{
-				"doc_count": map[string]interface{}{
-					"value": 2,
-				},
-			},
-		},
-		{
-			"a": "2022-02-24T08:00:00.000",
-			"aggs": map[string]interface{}{
-				"doc_count": map[string]interface{}{
-					"value": 12,
-				},
-			},
-		},
-	})
-
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-
-	val, ok = res["2022-02-24T05:00:00.000"]
-	if !ok {
-		t.FailNow()
-	}
-	if val.(int) != 2 {
-		t.Error(err)
-		t.Logf("Result: %d, Expected: %d", val.(int), 2)
-	}
-
 }
