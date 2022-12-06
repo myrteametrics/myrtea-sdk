26a860c4243a3d3f287ced1abf6870998af7eff5
cedricablain <ablain.cedric@gmail.com>
2022-12-06 16:40:14 +0100

    minor fixes

--- connector/source_kafka.go
+++ connector/source_kafka.go
@@ -6,8 +6,7 @@ import (
 	cluster "github.com/bsm/sarama-cluster"
 	"go.uber.org/zap"
 )
-
-//KafkaSource ...
+KafkaSource ...
 type KafkaSource struct {
 	Topic     string
 	Consumer  *cluster.Consumer
@@ -15,7 +14,6 @@ type KafkaSource struct {
 	Signals   chan os.Signal
 }
 
-//NewKafkaSource ...
 func NewKafkaSource(topic string, consumer *cluster.Consumer, out chan Message, signals chan os.Signal) *KafkaSource {
 	source := KafkaSource{
 		Topic:     topic,
@@ -26,41 +24,29 @@ func NewKafkaSource(topic string, consumer *cluster.Consumer, out chan Message, 
 	return &source
 }
 
-//Init ...
 func (src *KafkaSource) Init() error {
 	return nil
 }
 
-//Get ...
-/* func (src *KafkaSource) Get() []connectors.Message {
-	v := make([]connectors.Message, 0)
-	return v
-} */
-
-//Close ...
 func (src *KafkaSource) Close() error {
 	return nil
 }
 
-//Run ...
 func (src *KafkaSource) Run() error {
 	zap.L().Sugar().Infof("Starting consumer on topic=%s", src.Topic)
 
-	// consume errors
 	go func() {
 		for err := range src.Consumer.Errors() {
 			zap.L().Error("Error:", zap.Error(err))
 		}
 	}()
 
-	// consume notifications
 	go func() {
 		for ntf := range src.Consumer.Notifications() {
 			zap.L().Sugar().Infof("Rebalanced: %+v\n", ntf)
 		}
 	}()
 
-	// consume messages
 	for {
 		select {
 		case msg, ok := <-src.Consumer.Messages():
