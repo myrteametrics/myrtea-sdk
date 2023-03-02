21003f931777ab28585f4bcf6f86a6588e5ce060
cedricablain <ablain.cedric@gmail.com>
2023-03-02 13:53:35 +0100

    Fix gval contains function

--- expression/functions_slice.go
+++ expression/functions_slice.go
@@ -6,20 +6,27 @@ func contains(arguments ...interface{}) (bool, error) {
 	if len(arguments) != 2 {
 		return false, fmt.Errorf("contains() expects exactly two arguments")
 	}
-	arg1, ok1 := arguments[0].([]interface{})
+
 	arg2, ok2 := arguments[1].(string)
-	if !ok1 || !ok2 {
+	if !ok2 {
 		return false, fmt.Errorf("contains() expects exactly one []string argument and one string argument")
 	}
 
-	for _, arg := range arg1 {
-		if s, ok := arg.(string); ok {
-			if s == arg2 {
-				return true, nil
+	switch arg1 := arguments[0].(type) {
+	case string:
+		return arg1 == arg2, nil
+
+	case []interface{}:
+		for _, arg := range arg1 {
+			if s, ok := arg.(string); ok {
+				if s == arg2 {
+					return true, nil
+				}
 			}
 		}
+	default:
+		return false, fmt.Errorf("contains() expects exactly one []string argument and one string argument")
 	}
 
 	return false, nil
-
 }
