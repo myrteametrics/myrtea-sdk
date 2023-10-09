34dc7cf711fad9ffc3f6d359239d10a1c0b4c3ae
SchawnnDev <iperskill@gmail.com>
2023-10-09 16:29:22 +0200

    added format variable to model field

--- modeler/model_test.go
+++ modeler/model_test.go
@@ -40,12 +40,12 @@ var model = Model{
 	},
 }
 
-var expectedModel = strings.ReplaceAll(`{"id":1,"name":"model-1","synonyms":["model","other"],"fields":[{"name":"f1","type":"int","semantic":false,"synonyms":["f1","f1other"]},
-{"name":"f2","type":"string","semantic":false,"synonyms":["f2","f2other"]},{"name":"f3","type":"datetime","semantic":false,"synonyms":["f3","f3other"]},
-{"name":"f4","type":"boolean","semantic":false,"synonyms":["f4","f4other"]},{"name":"f5","type":"object","keepObjectSeparation":false,
-"fields":[{"name":"a","type":"int","semantic":false,"synonyms":["a","aother"]},{"name":"b","type":"string","semantic":false,"synonyms":["b","bother"]}]},
-{"name":"f6","type":"object","keepObjectSeparation":true,"fields":[{"name":"a","type":"int","semantic":false,"synonyms":["a","aother"]},
-{"name":"b","type":"string","semantic":false,"synonyms":["b","bother"]}]}],"elasticsearchOptions":{"rollmode":"cron","rollcron":"0 0 * * *",
+var expectedModel = strings.ReplaceAll(`{"id":1,"name":"model-1","synonyms":["model","other"],"fields":[{"name":"f1","type":"int","format":"","semantic":false,"synonyms":["f1","f1other"]},
+{"name":"f2","type":"string","format":"","semantic":false,"synonyms":["f2","f2other"]},{"name":"f3","type":"datetime","format":"","semantic":false,"synonyms":["f3","f3other"]},
+{"name":"f4","type":"boolean","format":"","semantic":false,"synonyms":["f4","f4other"]},{"name":"f5","type":"object","keepObjectSeparation":false,
+"fields":[{"name":"a","type":"int","format":"","semantic":false,"synonyms":["a","aother"]},{"name":"b","type":"string","format":"","semantic":false,"synonyms":["b","bother"]}]},
+{"name":"f6","type":"object","keepObjectSeparation":true,"fields":[{"name":"a","type":"int","format":"","semantic":false,"synonyms":["a","aother"]},
+{"name":"b","type":"string","format":"","semantic":false,"synonyms":["b","bother"]}]}],"elasticsearchOptions":{"rollmode":"cron","rollcron":"0 0 * * *",
 "enablePurge":true,"purgeMaxConcurrentIndices":30,"patchAliasMaxIndices":2,"advancedSettings":{"number_of_replicas":0,"number_of_shards":1,"test":{"value":5}}}}`, "\n", "")
 
 func TestMarshal(t *testing.T) {
@@ -75,4 +75,73 @@ func TestMarshal(t *testing.T) {
 		t.Log(string(newB))
 		t.Log(string(b))
 	}
+}
+
+func TestSource(t *testing.T) {
+
+	for _, field := range model.Fields {
+		_, content := field.Source()
+		if len(content) == 0 {
+			t.Error("expected content")
+		}
+
+		var ok bool
+		var name string
+		var fieldType FieldType
+
+		switch field.(type) {
+		case *FieldObject:
+			obj := field.(*FieldObject)
+			name, ok = content["type"].(string)
+			fieldType = obj.Ftype
+			break
+		case *FieldLeaf:
+			leaf := field.(*FieldLeaf)
+			name, ok = content["type"].(string)
+			fieldType = leaf.Ftype
+			break
+		default:
+			t.Error("expected *FieldLeaf or *FieldObject")
+			break
+		}
+
+		if !ok && fieldType == Object {
+			continue
+		} else if !ok {
+			t.Error("expected type in component map")
+		}
+
+		switch fieldType {
+		case Int:
+			if name != "integer" {
+				t.Error("expected integer")
+			}
+			break
+		case String:
+			if name != "keyword" {
+				t.Error("expected keyword")
+			}
+			break
+		case Float:
+			if name != "float" {
+				t.Error("expected float")
+			}
+			break
+		case DateTime:
+			if name != "date" {
+				t.Error("expected date")
+			}
+			if len(content) != 2 {
+				t.Error("expected format date_hour_minute_second_millis")
+			}
+			break
+		case Boolean:
+			if name != "boolean" {
+				t.Error("expected boolean")
+			}
+			break
+		}
+
+	}
+
 }
