71a991add92cd7cf68dd5aa213e7e8347608ab77
ALI OMAR Ismail <liamsi2019@gmail.com>
2023-10-27 13:52:03 +0200

    Enhanced ReplaceKeysWithValues function to support multiple key formats
    - Updated the function to handle different key formats such as {{key}}+, +{{key}}, {{key}}, and +{{key}}+.
    - Added comprehensive unit tests to cover all these scenarios and ensure correctness.

--- variablesconfig/template_replacer.go
+++ variablesconfig/template_replacer.go
@@ -7,19 +7,24 @@ import (
 )
 
 func ReplaceKeysWithValues(m *map[string]string, variables map[string]string) {
-	for key, value := range *m {
-		updatedValue := strings.ReplaceAll(value, " ", "")
-		for varKey, varValue := range variables {
-			pattern := "+{{" + varKey + "}}+"
-			if strings.Contains(updatedValue, pattern) {
-				updatedValue = strings.Replace(updatedValue, pattern, varValue, -1)
-			}
-		}
+    for key, value := range *m {
+        updatedValue := strings.ReplaceAll(value, " ", "")
+        for varKey, varValue := range variables {
+            pattern1 := "+{{" + varKey + "}}+"
+            pattern2 := "{{" + varKey + "}}+"
+            pattern3 := "+{{" + varKey + "}}"
+            pattern4 := "{{" + varKey + "}}"
+			
+            updatedValue = strings.ReplaceAll(updatedValue, pattern1, varValue)
+            updatedValue = strings.ReplaceAll(updatedValue, pattern2, varValue)
+            updatedValue = strings.ReplaceAll(updatedValue, pattern3, varValue)
+            updatedValue = strings.ReplaceAll(updatedValue, pattern4, varValue)
+        }
 
-		if strings.Contains(updatedValue, "+{{") && strings.Contains(updatedValue, "}}+") {
-			zap.L().Error("Error: Unrecognized variableConfi in", zap.Any("key", key), zap.Any("with value", updatedValue))
-		}
+        if strings.Contains(updatedValue, "+{{") || strings.Contains(updatedValue, "}}+") || strings.Contains(updatedValue, "{{") || strings.Contains(updatedValue, "}}") {
+            zap.L().Error("Error: Unrecognized variableConfi in", zap.Any("key", key), zap.Any("with value", updatedValue))
+        }
 
-		(*m)[key] = updatedValue
-	}
+        (*m)[key] = updatedValue
+    }
 }
--- variablesconfig/template_replacer_test.go
+++ variablesconfig/template_replacer_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+
 func TestReplaceKeysWithValues(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -55,6 +56,76 @@ func TestReplaceKeysWithValues(t *testing.T) {
 			ReplaceKeysWithValues(&tt.input, tt.vars)
 			if !reflect.DeepEqual(tt.input, tt.expected) {
 				t.Errorf("replaceKeysWithValues() = %v, want %v", tt.input, tt.expected)
+			}
+		})
+	}
+}
+
+
+func TestReplaceKeysWithValuesAllPattern(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    map[string]string
+		vars     map[string]string
+		expected map[string]string
+	}{
+		{
+			name: "format {{key}}+ replacement",
+			input: map[string]string{
+				"lien_adel": "https://{{keyUnique}}+url",
+			},
+			vars: map[string]string{
+				"keyUnique": "url.exemple.eu",
+			},
+			expected: map[string]string{
+				"lien_adel": "https://url.exemple.euurl",
+			},
+		},
+		{
+			name: "format +{{key}} replacement",
+			input: map[string]string{
+				"lien_adel": "https://+{{keyUnique}}url",
+			},
+			vars: map[string]string{
+				"keyUnique": "url.exemple.eu",
+			},
+			expected: map[string]string{
+				"lien_adel": "https://url.exemple.euurl",
+			},
+		},
+		{
+			name: "format {{key}} replacement",
+			input: map[string]string{
+				"lien_adel": "https://{{keyUnique}}url",
+			},
+			vars: map[string]string{
+				"keyUnique": "url.exemple.eu",
+			},
+			expected: map[string]string{
+				"lien_adel": "https://url.exemple.euurl",
+			},
+		},
+		{
+			name: "multiple formats replacement",
+			input: map[string]string{
+				"lien_adel": "https://{{key1}}+url+{{key2}}domain+{{key3}}",
+			},
+			vars: map[string]string{
+				"key1": "firstPart",
+				"key2": "secondPart",
+				"key3": "thirdPart",
+			},
+			expected: map[string]string{
+				"lien_adel": "https://firstParturlsecondPartdomainthirdPart",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ReplaceKeysWithValues(&tt.input, tt.vars)
+			if !reflect.DeepEqual(tt.input, tt.expected) {
+				t.Errorf("replaceKeysWithValues() = %v, want %v", tt.input, tt)
 			}
 		})
 	}
