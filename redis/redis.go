bdf9ca3844bbfb17a51ccafebca554b5a7356bfb
SchawnnDev <iperskill@gmail.com>
2024-06-12 15:53:01 +0200

    Removed pointer for redis client singleton instance

--- redis/redis.go
+++ redis/redis.go
@@ -6,16 +6,16 @@ import (
 	"go.uber.org/zap"
 )
 
-var _globalC *rueidis.Client
+var _globalC rueidis.Client
 
 // C returns a pointer the singleton instance of rueidis.Client
-func C() *rueidis.Client {
+func C() rueidis.Client {
 	return _globalC
 }
 
 // ReplaceGlobals replaces the global Logger and SugaredLogger, and returns a
 // function to restore the original values. It's safe for concurrent use.
-func ReplaceGlobals(redis *rueidis.Client) func() {
+func ReplaceGlobals(redis rueidis.Client) func() {
 	prev := _globalC
 	_globalC = redis
 	return func() { ReplaceGlobals(prev) }
